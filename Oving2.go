0aa9fcb6b9cbf9642e4977f5e08fa359ede1a748
JorgenW <jorgen.waloen@gmail.com>
2014-01-23 11:10:18 +0100

    Update Oving2.go

--- Oving2.go
+++ Oving2.go
@@ -6,33 +6,40 @@ import (
     . "time"
 )
 
-var i = 0
-var p="0"
-var message=make(chan string,1)
-var m=""
-func adder() {
-	
-    for x := 0; x < 1000000; x++ {
-		message<- "1"
-        i++
-		p=<-message
-    }
+//var i = 0
+//var p="0"
+//var message=make(chan string,1)
+//var m=""
+
+func adder(i chan int) {
+        
+	for x := 0; x < 1000010; x++ {
+                c := <- i
+        	c++
+		
+                i <- c
+	}
 }
-func adder2(){
-    for x := 0; x < 1000000; x++ {
-		message<- "1"
-        i--
-		p=<-message
-		//Println(<-message)
-    }
+func adder2(i chan int){
+	
+	for x := 0; x < 1000000; x++ {
+                d := <- i
+                d--
+		
+                i <- d
+	}
+
 }
 
 func main() {
-    GOMAXPROCS(NumCPU()) // I guess this is a hint to what GOMAXPROCS does...
-    go adder() // This spawns adder() as a goroutine
-    go adder2()
-    // No way to wait for the completion of a goroutine (without additional syncronization)
-    // We'll come back to using channels in Exercise 2. For now: Sleep
-    Sleep(100*Millisecond)
-    Println("Done:", i);
+	var i chan int = make(chan int, 1)
+	i <- 0
+	GOMAXPROCS(NumCPU()) // I guess this is a hint to what GOMAXPROCS does...
+	go adder(i) // This spawns adder() as a goroutine
+	go adder2(i)
+	// No way to wait for the completion of a goroutine (without additional syncronization)
+	// We'll come back to using channels in Exercise 2. For now: Sleep
+	Sleep(Millisecond * 500)
+	a := <-i
+	Println("Done:", a);
 }
